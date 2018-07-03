159838c9c89fbd0515f7d334be5390d35c1c6249
JohnMark <johnmarkludwick@gmail.com>
2018-07-03 16:47:22 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -15,12 +15,12 @@ class Owner
     @@all
   end
 
-  def self.count
-    @@all.size
-  end
-  
-  def self.reset
+  def self.reset_all
     @@all.clear
+  end
+
+  def self.count 
+    @@all.size
   end
 
 
